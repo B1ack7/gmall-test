35c7888329471a49c3557d8f0b4aeae0be31d33d
lanzhenghong <790873711@qq.com>
2020-01-06 23:14:34 +0800

    项目结构完成。。。

---
+++ gmall-admin-web/src/main/java/cn/lzh/gmall/ladmin/GmallAdminWebApplication.java
@@ -0,0 +1,13 @@
+package cn.lzh.gmall.ladmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallAdminWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallAdminWebApplication.class, args);
+    }
+
+}
